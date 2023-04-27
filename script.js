5f3baf7eca2dfa40c527d7ac5b8cb8d4adf98296
Harsh Kumar <Harshkr70@gmail.com>
2023-04-27 19:27:36 +0530

    Create 16*16 grid and added borders to it

---
+++ script.js
@@ -0,0 +1,22 @@
+const row= function()
+{
+    const container=document.getElementById('container');
+    
+    for(let i=0;i<16;i++){
+        const row= document.createElement('div');
+        row.classList.add('rows');
+        container.appendChild(row);
+    }
+    const arr= document.querySelectorAll('.rows')
+    
+    for(const item of arr) {
+
+        for(let i=0;i<16;i++) {
+           const coulmn=document.createElement('div');
+           coulmn.classList.add('coulm');
+           item.appendChild(coulmn);
+        }
+    }
+    
+}
+row();
