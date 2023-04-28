16d5ff6a3514714aceef8334f35e300e34ff746e
Harsh Kumar <Harshkr70@gmail.com>
2023-04-28 20:52:33 +0530

    Commitin randomly

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
-const row= function()
-{
-    const container=document.getElementById('container');
-    
-    for(let i=0;i<16;i++){
-        const row= document.createElement('div');
+const row = function (num) {
+    const container = document.getElementById('container');
+
+    for (let i = 0; i < num; i++) {
+        const row = document.createElement('div');
         row.classList.add('rows');
         container.appendChild(row);
     }
-    const arr= document.querySelectorAll('.rows')
-    
-    for(const item of arr) {
+    const arr = document.querySelectorAll('.rows')
 
-        for(let i=0;i<16;i++) {
-           const coulmn=document.createElement('div');
-           coulmn.classList.add('coulm');
-           item.appendChild(coulmn);
+    for (const item of arr) {
+        const input = document.querySelector('input');
+        const btn = document.querySelector('.btn');
+        const grid = document.querySelector('.grid');
+        for (let i = 0; i < num; i++) {
+            const coulmn = document.createElement('div');
+            coulmn.classList.add('coulm');
+            item.appendChild(coulmn);
+
         }
     }
+
+}
+row(16);
+const remove=function()
+{
     
 }
 
 
+const gridnum = function () {
+    const input = document.querySelector('input');
+    const btn = document.querySelector('.btn');
+    const grid = document.querySelector('.grid');
+    btn.addEventListener('click', () => {
+        if (input.value < 0 || input.value > 40) {
+            const message = document.createElement('p');
+            message.classList.add('message');
+            message.textContent = 'Enter something in between 1 and 40';
+            grid.appendChild(message);
 
-
-row();
+        }
+        else {
+            row(input.value);
+        }
+    })
+}
+gridnum();
