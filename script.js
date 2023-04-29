3f7cffd41de39f8b6d805e23b35511dd18499ae5
Harsh Kumar <Harshkr70@gmail.com>
2023-04-29 10:21:13 +0530

    added basic hover effect that will up a certain block of elements when mouse is hovered over it

--- script.js
+++ script.js
@@ -18,22 +18,30 @@ const row = function (num) {
     }
 
 }
-row(16);
 const emptyHTMl = function (parentnode) {
     parentnode.innerHTML = "";
 }
+const reset = function () {
+    const container = document.getElementById('container');
+    const Reset = document.querySelector('.reset');
+    Reset.addEventListener('click', () => {
+        emptyHTMl(container);
+        row(16);
+    })
+}
 const gridnum = function () {
     const container = document.getElementById('container');
-
     const input = document.querySelector('input');
     const btn = document.querySelector('.btn');
+    const color = document.querySelector('.color')
     const grid = document.querySelector('.grid');
     btn.addEventListener('click', () => {
-        if (input.value < 0 || input.value > 40) {
-            const message = document.createElement('p');
-            message.classList.add('message');
-            message.textContent = 'Enter something in between 1 and 40';
-            grid.appendChild(message);
+        if (input.value <= 0 || input.value > 40) {
+            // const message = document.createElement('p');
+            // message.classList.add('message');
+            // message.textContent = 'Enter something in between 1 and 40';
+            // grid.insertBefore(message,color);
+            alert('Enter something in between 1 and 40');
         }
         else {
             //to empty inner HTML Here what we  need to do is :
@@ -43,4 +51,24 @@ const gridnum = function () {
 
     })
 }
-gridnum();
+const colorChange = function (item) {
+    item.addEventListener('mouseover', () => {
+        item.style.backgroundColor ="black";
+    })
+}
+
+const addColor = function () {
+    //all 256 divs are selected here not to add touch function here 
+    const block = document.querySelectorAll('.coulm');
+    block.forEach(colorChange);
+}
+
+
+
+const run = function () {
+    row(16);
+    reset();
+    gridnum();
+    addColor();
+}
+run();
